f18bcd7d3e317b191ca4e167dfc8ea1ed2f0129d
Martins100-Tmd <olaitanfabayo@gmail.com>
2025-07-11 08:32:22 +0100

    Working...

--- src/user/profile.tsx
+++ src/user/profile.tsx
@@ -1,5 +1,27 @@
 export default function UserProfile() {
   return (
-    <section className="w-full bg-white h-screen sm:bg-gray-200 flex justify-center"></section>
+    <section className="w-full bg-white h-screen sm:bg-gray-200 flex justify-center">
+      <section className="w-full bg-white self-center sm:h-auto sm:w-1/2 mx-auto rounded-sm flex flex-col items-center gap-2 p-3">
+        <div className="flex flex-row items-center w-full justify-start">
+          <p className="sm:text-lg font-all font-medium">Profile Details</p>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="lucide lucide-user-pen-icon lucide-user-pen"
+          >
+            <path d="M11.5 15H7a4 4 0 0 0-4 4v2" />
+            <path d="M21.378 16.626a1 1 0 0 0-3.004-3.004l-4.01 4.012a2 2 0 0 0-.506.854l-.837 2.87a.5.5 0 0 0 .62.62l2.87-.837a2 2 0 0 0 .854-.506z" />
+            <circle cx="10" cy="7" r="4" />
+          </svg>
+        </div>
+      </section>
+    </section>
   );
 }
