31f458ca076792c604abba059b7f10997e59e3bf
Martins100-Tmd <olaitanfabayo@gmail.com>
2025-07-22 19:21:21 +0100

    Working

--- src/checkout/product.tsx
+++ src/checkout/product.tsx
@@ -526,37 +526,6 @@ function DeliveryOption({
   );
 }
 
-function PaymentOption({
-  value,
-  checked,
-  onChange,
-  label,
-  extra,
-}: {
-  value: any;
-  checked: any;
-  onChange: any;
-  label: any;
-  extra: any;
-}) {
-  return (
-    <div className="flex items-center justify-between w-full py-3 border-b border-stone-300">
-      <label className="flex items-center space-x-3 cursor-pointer">
-        <input
-          type="radio"
-          name="payment"
-          value={value}
-          checked={checked}
-          onChange={(e) => onChange(e.target.value)}
-          className="w-4 h-4 text-green-600 border border-green-700 appearance-none rounded-full checked:bg-green-600"
-        />
-        <span className="text-sm font-all font-medium">{label}</span>
-      </label>
-      {extra && <div className="flex items-center">{extra}</div>}
-    </div>
-  );
-}
-
 function FormField({
   label,
   value,
