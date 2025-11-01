06578f0594d08ee467519d8df63937b637cc57e5
Martins100-Tmd <olaitanfabayo@gmail.com>
2025-11-01 14:03:48 +0100

    updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,14 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 
 window.addEventListener("contextmenu", function (e) {
   e.preventDefault();
 });
 
 createRoot(document.getElementById("root")!).render(
-  <GoogleOAuthProvider clientId="866408853892-kdh6vkhasr1c832buvt6fdmvtttf42i4.apps.googleusercontent.com">
-    <StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </StrictMode>
-  </GoogleOAuthProvider>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
